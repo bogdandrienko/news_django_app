700f5198eb7f4ceb7e0ef320b91592a2dfe9861f
Bogdan <67187037+bogdandrienko@users.noreply.github.com>
2023-08-24 22:03:05 +0600

    update

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from app import models
+admin.site.register(models.News)
+admin.site.register(models.Category)
 # Register your models here.
--- app/models.py
+++ app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+class Category(models.Model):
+    title = models.CharField(max_length=300, verbose_name="Наименование")
+    def __str__(self):
+        return self.title
+
+class News(models.Model):
+    title = models.CharField(max_length=300, verbose_name="Наименование")
+    category = models.ForeignKey(verbose_name="Категория", to=Category, on_delete=models.CASCADE, null=True, blank=True, default=None)
+    def __str__(self):
+        return self.title
 
 # Create your models here.
